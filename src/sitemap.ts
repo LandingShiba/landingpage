e78d1bad1e773b3b1a995abbae5667a05fc498c1
Devtu32 <nguyendinhtu11022002@gmail.com>
2025-10-27 21:37:12 +0700

    updaTE

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -8,26 +8,61 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: new URL("/", base).toString(),
             changeFrequency: "weekly",
             priority: 1,
+            alternates: {
+                languages: {
+                    en: `${base}/en`,
+                    vi: `${base}/vi`,
+                    ja: `${base}/ja`,
+                },
+            },
         },
         {
             url: new URL("/company", base).toString(),
             changeFrequency: "monthly",
             priority: 0.6,
+            alternates: {
+                languages: {
+                    en: `${base}/en`,
+                    vi: `${base}/vi`,
+                    ja: `${base}/ja`,
+                },
+            },
         },
         {
             url: new URL("/privacy", base).toString(),
             changeFrequency: "yearly",
             priority: 0.4,
+            alternates: {
+                languages: {
+                    en: `${base}/en`,
+                    vi: `${base}/vi`,
+                    ja: `${base}/ja`,
+                },
+            },
         },
         {
             url: new URL("/contact", base).toString(),
             changeFrequency: "monthly",
             priority: 0.7,
+            alternates: {
+                languages: {
+                    en: `${base}/en`,
+                    vi: `${base}/vi`,
+                    ja: `${base}/ja`,
+                },
+            },
         },
         {
             url: new URL("/work", base).toString(),
             changeFrequency: "monthly",
             priority: 0.5,
+            alternates: {
+                languages: {
+                    en: `${base}/en`,
+                    vi: `${base}/vi`,
+                    ja: `${base}/ja`,
+                },
+            },
         },
     ];
 }
